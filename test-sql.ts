a102296783f7719748ba749aeaeda602e7773e3d
SumitPujari30 <168459589+SumitPujari30@users.noreply.github.com>
2026-03-24 13:58:00 +0530

    chore: prepare for Vercel deployment - secure .env, ignore build errors

--- test-sql.ts
+++
@@ -1,7 +0,0 @@
-import { supabaseAdmin } from './src/lib/supabase/admin'
-
-async function run() {
-  const { data, error } = await supabaseAdmin.rpc('exec_sql', { sql: 'SELECT 1' })
-  console.log('Result:', data, error)
-}
-run()
